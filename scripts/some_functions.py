bdffde5737c28bd9ba7c26c86d86f6dff8c2e67d
Lughaigh-w <fyf105@hotmail.com>
2024-04-15 17:50:02 +0100

    retention bootstrapping

--- scripts/some_functions.py
+++ scripts/some_functions.py
@@ -1,20 +1,22 @@
+## contains some basic functions ##
+
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
-
-#def grid_plot(grid_x, grid_y):
-
+import pandas as pd
 
 
 basic_colour = sns.xkcd_rgb['deep red']
+basic_colour_2 = sns.xkcd_rgb['royal']
+
+basic_pallette = [basic_colour, basic_colour_2]
+
 
 # takes a series
-def basic_histo(data, colour=basic_colour, **kwargs):
+def basic_histo(data, values="Values", colour=basic_colour, **kwargs):
     plt.figure(figsize=(6, 4))
     sns.histplot(data, color=colour, **kwargs)
     
-    plt.xlabel('Values')
+    plt.xlabel(values)
     plt.ylabel('Frequency')
 
     plot_title=data.name
@@ -22,16 +24,34 @@ def basic_histo(data, colour=basic_colour, **kwargs):
 
     plt.show()
 
+    return plt
+
 
 # takes a series
-def basic_boxplot(data, colour=basic_colour, **kwargs):
-    plt.figure(figsize=(8, 6))  
+def basic_boxplot(data, values="Values", colour=basic_colour, **kwargs):
+    plt.figure(figsize=(6, 4))  
     sns.boxplot(data=data, color=colour, **kwargs)
 
-    plt.xlabel('Values')
+    plt.xlabel(values)
     plt.ylabel('Frequency')
 
     plot_title = data.name
     plt.title(f'Boxplot of {plot_title}')
 
     plt.show()
+
+    return plt
+
+
+def basic_bootstrap(data, sample_n=100, x_value="retention_1", labels="version" ):
+    boot_list = []
+    for i in range(sample_n):
+        boot_mean = data.sample(frac=1, replace=True).groupby(labels)[x_value].mean()
+        boot_list.append(boot_mean)
+    
+    boot_df = pd.DataFrame(boot_list)
+    
+    sns.kdeplot(data=boot_df, fill=True, palette=basic_pallette)
+    plt.title(f"Bootstrap of {x_value} with {sample_n} samples")
+    plt.show()
+              
