3ad67b714cc5ff4645f34b94354c96a81f5c40aa
Lughaigh-w <fyf105@hotmail.com>
2024-04-16 13:14:58 +0100

    some statitsical models

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AB Testing
----
+This project performs A/B testing on player retention data for a mobile game.
+
+A selection of appropriate statistical tests are performed on the data.
 
--- scripts/some_functions.py
+++ scripts/some_functions.py
@@ -8,39 +8,32 @@ import pandas as pd
 basic_colour = sns.xkcd_rgb['deep red']
 basic_colour_2 = sns.xkcd_rgb['royal']
 
-basic_pallette = [basic_colour, basic_colour_2]
+basic_pallette = [basic_colour_2, basic_colour]
 
 
 # takes a series
 def basic_histo(data, values="Values", colour=basic_colour, **kwargs):
     plt.figure(figsize=(6, 4))
     sns.histplot(data, color=colour, **kwargs)
-    
-    plt.xlabel(values)
-    plt.ylabel('Frequency')
 
+    plt.xlabel(values)
+    
     plot_title=data.name
     plt.title(f'Distribution of {plot_title}')
 
-    plt.show()
-
-    return plt
+    plt.show();
 
 
 # takes a series
-def basic_boxplot(data, values="Values", colour=basic_colour, **kwargs):
+def basic_boxplot(data, values=None, colour=basic_colour, **kwargs):
     plt.figure(figsize=(6, 4))  
     sns.boxplot(data=data, color=colour, **kwargs)
 
     plt.xlabel(values)
-    plt.ylabel('Frequency')
 
     plot_title = data.name
     plt.title(f'Boxplot of {plot_title}')
-
-    plt.show()
-
-    return plt
+    plt.show();
 
 
 def basic_bootstrap(data, sample_n=100, x_value="retention_1", labels="version" ):
@@ -52,6 +45,7 @@ def basic_bootstrap(data, sample_n=100, x_value="retention_1", labels="version" 
     boot_df = pd.DataFrame(boot_list)
     
     sns.kdeplot(data=boot_df, fill=True, palette=basic_pallette)
+    plt.ylabel(None)
+
     plt.title(f"Bootstrap of {x_value} with {sample_n} samples")
-    plt.show()
-              
+    plt.show();
